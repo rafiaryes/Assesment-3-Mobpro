359117a630b332e846e969726f92f72e227266d1
rafiaryes <rafiaryaramadhan2710@gmail.com>
2025-06-12 22:22:01 +0700

    Menambahkan model user

---
+++ app/src/main/java/com/rafiarya0114/katalogmotor/model/User.kt
@@ -0,0 +1,8 @@
+package com.rafiarya0114.katalogmotor.model
+
+data class User(
+    val token: String = "",
+    val name: String = "",
+    val email: String = "",
+    val photoUrl: String = ""
+)
