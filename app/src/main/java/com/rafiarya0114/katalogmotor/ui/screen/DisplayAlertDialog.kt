93621fd52ce4043985a44f2c5a312c3ff1899070
rafiaryes <rafiaryaramadhan2710@gmail.com>
2025-06-12 22:32:47 +0700

    menambahkan displayalertdialog untuk dialog hapus, katalog dialog untuk tambah dan update, dan profile dialog untuk menampilkan user yagn sedang login

---
+++ app/src/main/java/com/rafiarya0114/katalogmotor/ui/screen/DisplayAlertDialog.kt
@@ -0,0 +1,36 @@
+package com.rafiarya0114.katalogmotor.ui.screen
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.rafiarya0114.katalogmotor.R
+import com.rafiarya0114.katalogmotor.model.Katalog
+
+@Composable
+fun DisplayAlertDialog(
+    katalog: Katalog,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = {
+            Text(text = stringResource(R.string.hapus_hewan_title))
+        },
+        text = {
+            Text(text = stringResource(R.string.hapus_hewan_body, katalog.judul))
+        },
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text(text = stringResource(R.string.hapus))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text(text = stringResource(R.string.batal))
+            }
+            }
+        )
+}
