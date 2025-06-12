d84e3d4ad724081b0126dfcea1d1c9e3f5050dec
rafiaryes <rafiaryaramadhan2710@gmail.com>
2025-06-12 22:37:31 +0700

    memindahkan fitur bitmap cropp image dari mainscreen ke katalogdialog, agar dapat digunakan sebagai opsional saat update

--- app/src/main/java/com/rafiarya0114/katalogmotor/ui/screen/KatalogDialog.kt
+++ app/src/main/java/com/rafiarya0114/katalogmotor/ui/screen/KatalogDialog.kt
@@ -1,7 +1,13 @@
 package com.rafiarya0114.katalogmotor.ui.screen
 
+import android.content.ContentResolver
 import android.content.res.Configuration
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -24,24 +30,43 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageOptions
+import com.canhub.cropper.CropImageView
 import com.rafiarya0114.katalogmotor.R
+import com.rafiarya0114.katalogmotor.model.Katalog
+import com.rafiarya0114.katalogmotor.network.KatalogApi
 import com.rafiarya0114.katalogmotor.ui.theme.AboutMeTheme
 
 @Composable
-fun HewanDialog(
-    bitmap: Bitmap?,
+fun KatalogDialog(
+    katalog: Katalog? = null,
     onDismissRequest: () -> Unit,
-    onConfirmation: (String, String) -> Unit
+    onConfirmation: (String, String, Double, Bitmap) -> Unit
 ) {
+    val context = LocalContext.current
+
     var judul by remember { mutableStateOf("") }
     var manufacturer by remember { mutableStateOf("") }
     var harga by remember { mutableDoubleStateOf(0.0) }
+
+    var bitmap: Bitmap? by remember { mutableStateOf(null) }
+    val launcher = rememberLauncherForActivityResult(CropImageContract()) {
+        bitmap = getCroppedImage(context.contentResolver, it)
+    }
+
     Dialog(onDismissRequest = { onDismissRequest() }) {
         Card(
             modifier = Modifier.padding(16.dp),
@@ -51,11 +76,44 @@ fun HewanDialog(
                 modifier = Modifier.padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Image(
-                    bitmap = bitmap!!.asImageBitmap(),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxWidth().aspectRatio(1f)
-                )
+                if (katalog != null && bitmap == null) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(
+                                KatalogApi.getImageUrl(katalog.id_katalog)
+                            )
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = stringResource(R.string.gambar, katalog.judul),
+                        contentScale = ContentScale.Crop,
+                        placeholder = painterResource(id = R.drawable.loading_img),
+                        error = painterResource(id = R.drawable.broken_img),
+                        modifier = Modifier.fillMaxWidth().aspectRatio(1f)
+                    )
+                } else {
+                    if (bitmap != null) {
+                        Image(
+                            bitmap = bitmap!!.asImageBitmap(),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxWidth().aspectRatio(1f)
+                        )
+                    }
+                }
+                OutlinedButton(
+                    onClick = {
+                        val options = CropImageContractOptions(
+                            null, CropImageOptions(
+                                imageSourceIncludeGallery = false,
+                                imageSourceIncludeCamera = true,
+                                fixAspectRatio = true
+                            )
+                        )
+                        launcher.launch(options)
+                    },
+                    modifier = Modifier.padding(top = 8.dp).fillMaxWidth()
+                ) {
+
+                }
                 OutlinedTextField(
                     value = judul,
                     onValueChange = { judul = it },
@@ -101,15 +159,35 @@ fun HewanDialog(
     }
 }
 
+@Suppress("deprecation")
+private fun getCroppedImage(
+    resolver: ContentResolver,
+    result: CropImageView.CropResult
+): Bitmap? {
+    if (!result.isSuccessful) {
+        Log.e("IMAGE", "Error: ${result.error}")
+        return null
+    }
+
+    val uri = result.uriContent ?: return null
+
+    return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+        MediaStore.Images.Media.getBitmap(resolver, uri)
+    } else {
+        val source = ImageDecoder.createSource(resolver, uri)
+        ImageDecoder.decodeBitmap(source)
+    }
+}
+
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun AddDialogPreview() {
     AboutMeTheme {
-        HewanDialog(
-            bitmap = null,
+        KatalogDialog(
+            katalog = null,
             onDismissRequest = {},
-            onConfirmation = { _, _ -> }
+            onConfirmation = { _, _, _, _ -> }
         )
     }
 }
